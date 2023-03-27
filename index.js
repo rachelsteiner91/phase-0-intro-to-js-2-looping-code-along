b6c8da8ba71b42840656233c9ace2ddc935a6cfc
Rachel Steiner <steinerr310@gmail.com>
2023-03-27 14:56:41 -0400

    looping lab complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+let cards = [];
+
+function writeCards(names, surprise)  {
+  for (let i = 0; i < names.length; i++) {
+    cards.push(`Thank you, ${names[i]}, for the wonderful ${surprise} gift!`);
+  }
+  return cards;
+  }
+
+  writeCards(names);
+
+function countDown(positiveNumber){
+while(positiveNumber >= 0) {
+  console.log(positiveNumber);
+  positiveNumber--;
+}
+}
+ 
